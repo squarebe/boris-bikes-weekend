d425d75976648dd73d0612bbadcbf1bdc314724e
Balint Nemes <squarebe03@gmail.com>
2015-07-29 09:16:32 +0100

    commiting ch10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
+require_relative 'bike' #requires a file within the dir.
+
 class DockingStation
 
   def release_bike
-    
+    Bike.new #creates a bike-obj
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,12 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
+#  it { is_expected.to respond_to :working? }
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    #gotta build up objs for the test
+    expect(bike).to be_working
+  end
 
 end
