4907f0fe7c291ebbec29536f5bd4dabcb97aefc4
Balint Nemes <squarebe03@gmail.com>
2015-07-31 09:08:25 +0100

    1st post challenge done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,13 @@ require_relative 'bike' #requires a file within the dir.
 class DockingStation
 
   def release_bike
-    Bike.new #creates a bike-obj
+    # Bike.new #creates a bike-obj # later moved to the spec-file
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+
+  def dock bike
+  	@bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,12 +3,21 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
-#  it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  # when passing a n argument must be attached to the requrement
 
   it 'releases working bikes' do
+  	subject.dock Bike.new
     bike = subject.release_bike
     #gotta build up objs for the test
     expect(bike).to be_working
   end
 
+  describe '#release bike' do # #is an instance variable
+    it 'raises an error when no bike available' do
+    	expect { subject.release_bike }.to raise_error 'No bikes available'
+    	# {} makes the prg runs code inside first then the outside ones,
+    	# important at fail msg raise not to break the prg.
+    end
+  end
 end
