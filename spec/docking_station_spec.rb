ebf89443c334be6d1cea29e3f76ca45e51c5ad27
Balint Nemes <squarebe03@gmail.com>
2015-08-02 11:39:00 +0100

    weirly not all refactoring works :(

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,10 +7,13 @@ describe DockingStation do
   # when passing a n argument must be attached to the requrement
 
   it 'releases working bikes' do
-  	subject.dock Bike.new
+    subject.dock Bike.new
     bike = subject.release_bike
     #gotta build up objs for the test
     expect(bike).to be_working
+#    bike = double :bike, broken?: false
+#    subject.dock bike
+#    expect(subject.release_bike).to be bike
   end
 
   it 'has a default capacity' do
@@ -27,14 +30,14 @@ describe DockingStation do
     it 'do not release broken bikes' do
       bike = double :bike, working?: false
       subject.dock(bike)
-      expect { subject.release_bike}.to raise_error 'No bikes available'
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 
   describe 'dock' do
   	it 'raises an error when full' do
-  		20.times { subject.dock Bike.new }
-  		expect {subject.dock Bike.new}.to raise_error 'Docking station full'
+  		20.times { subject.dock :bike }
+  		expect {subject.dock :bike }.to raise_error 'Docking station full'
   	end
   end
 
