409fae96cba95d89796def86158af930395d8b67
Balint Nemes <squarebe03@gmail.com>
2015-08-02 09:34:06 +0100

    releasing bike fixed, refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,15 +8,27 @@ class DockingStation
 
   def release_bike
     # Bike.new #creates a bike-obj # later moved to the spec-file
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     # raises an error if bikes array is empty.
     @bikes.pop #release the last bike added.
   end
 
   def dock bike
-  	fail 'Docking station full' if @bikes.count >= 20
+  	fail 'Docking station full' if full?
   	# raises an error if bikes array more/equal the given number
   	@bikes << bike # it now appends bikes into the array
+  end
+
+  private
+
+  def full?
+  	@bikes.count >= 20
+  	# heres setting the full capacity.
+  end
+
+  def empty?
+  	@bikes.empty?
+  	#heres defining what the empty array means
   end
 
 end
